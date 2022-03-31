1d7392e2715a7e9c4a33684e2566a09cc547004c
AS <18801240075@163.com>
2022-03-31 17:17:31 +0800

    AS

--- pagedb/src/test/java/astest/ExcelTest.java
+++ pagedb/src/test/java/astest/ExcelTest.java
@@ -1,7 +1,7 @@
 package astest;
 
 import astest.aboutexcel.AboutExcel;
-import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -10,10 +10,10 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
-import pers.as.pagedb.entity.novel.Novel;
 
 import java.io.*;
 import java.math.BigDecimal;
+import java.text.FieldPosition;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -26,124 +26,49 @@ public class ExcelTest {
     @Test
     public void excelfunTest (){
         List<Object> list = AboutExcel.getExcelList("C:\\Users\\AS\\Desktop\\aspagesql\\novels.xlsx",0,0,0,0);
-        AboutExcel.outputListValue(list);
+        //AboutExcel.outputListValue(list);
+        String path = ".\\src\\test\\testfile\\novels2.xlsx";
+        AboutExcel.writeExcel(path,list);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     @Test
-    public void importByExcel() {
-        String pathname = "C:\\Users\\AS\\Desktop\\aspagesql\\novels.xlsx";
-        int sheetnum = 0;
-        System.out.println(pathname);
-        File file = new File(pathname);
-        List<Object> list = new ArrayList();
+    public void testfun() throws IOException, InvalidFormatException {
+        String path = ".\\src\\test\\testfile\\novels2.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(path);
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        XSSFRow row = sheet.createRow(0);
+        XSSFCell cell = row.createCell(0);
+        cell.setCellValue("ccxs");
+        FileOutputStream out = new FileOutputStream(new File(path));
+        workbook.write(out);
+        workbook.close();
+        System.out.println(System.getProperty("user.dir"));
+
+    }
+    @Test
+    public void test2() throws IOException {
+        String filePath = ".\\src\\test\\testfile\\novels2.xlsx";
+        // 输出流
         try {
-            InputStream inputStream = new FileInputStream(file);
-            Workbook workbook = new XSSFWorkbook(inputStream);
-            //XSSFSheet sheet = (XSSFSheet) workbook.getSheetAt(1);
-            Sheet sheet = workbook.getSheetAt(sheetnum);
-            for (int i = 0; i < sheet.getLastRowNum(); i++) {
-                Row row = sheet.getRow(i);
-                List<Object> listcell = new ArrayList();
-                if (row == null) {
-                    list.add(null);
-                    continue;
-                }
-                for (int j = 0; j < row.getLastCellNum(); j++) {
-
-                    Cell cell = row.getCell(j);
-                    listcell.add(getValve(cell));
-
-                }
-                list.add(listcell);
-            }
-
-        } catch (FileNotFoundException e) {
+            FileInputStream is = new FileInputStream(filePath);
+            Workbook wb = new XSSFWorkbook(is);
+            //System.out.println("aaa");
+            Sheet sheet1 = wb.getSheetAt(0);//获取第一sheet页
+            //System.out.println(sheet1.getLastRowNum());
+            Row row = sheet1.getRow(sheet1.getLastRowNum() -2);
+            row.setHeightInPoints((short) 25);
+            // 给这一行赋值
+            row.createCell(0).setCellValue("12");
+            row.createCell(1).setCellValue("23");
+            FileOutputStream os = new FileOutputStream(filePath);
+            wb.write(os);
+            is.close();
+            os.close();
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        outputListValue(list);
 
-    }
-
-    /**
-     * 在控制台打印一个列表
-     * @param list 输入为一个list<object>
-     */
-    public void outputListValue(List<Object> list) {
-        // System.out.println(list);
-        for (int i = 0; i < list.size(); i++) {
-            List listcell = (List) list.get(i);
-            for (int j = 0; j < listcell.size(); j++) {
-                System.out.print(listcell.get(j) + " ");
-            }
-            System.out.println();
         }
     }
 
-    /**
-     * 获取excel 的单元格的值
-     * @param cell 传入的excel 的单元格对象
-     * @return 返回一个Object对象，String,Bollean,Numeric,
-     */
-    public Object getValve(Cell cell) {
-        if (cell == null)
-            return null;
-        Object value = null;
-        switch (cell.getCellType()) {
-            case BOOLEAN:
-                value = cell.getBooleanCellValue();
-                break;
-            case STRING:
-                value = cell.getStringCellValue();
-                break;
-            case NUMERIC:
-                value = new BigDecimal(cell.getNumericCellValue()) ;
-            case FORMULA:
-                switch (cell.getCachedFormulaResultType()){
-                    case NUMERIC:
-                        value = cell.getNumericCellValue();
-                        break;
-                    case STRING:
-                        value = cell.getRichStringCellValue();
-                    default:
-                        value = cell.toString();
-                        break;
-                }
-                break;
-            default:
-                value = cell.toString();
-                break;
-        }
-        return value;
-    }
-
-    /**
-     * 将一个一维或二维的list列表输出到一个Excel中
-     * @param list 输入的List
-     */
-    public void outputExcel(List<Object> list){
-
-    }
 
 }
--- pagedb/src/test/java/astest/aboutexcel/AboutExcel.java
+++ pagedb/src/test/java/astest/aboutexcel/AboutExcel.java
@@ -1,9 +1,13 @@
 package astest.aboutexcel;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
@@ -11,6 +15,12 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @ClassName: AboutExcel
+ * @description:
+ * @author: AS
+ * @create: 2022-03-31 16:38
+ */
 public class AboutExcel {
 
     /**
@@ -95,6 +105,7 @@ public class AboutExcel {
         }
         return value;
     }
+
     /**
      * 在控制台打印一个列表
      * @param list 输入为一个list<object>
@@ -110,5 +121,72 @@ public class AboutExcel {
         }
     }
 
+    /**
+     * 向excel文件中写入数据
+     * @param path excel地址
+     * @param list 数据二维list列表
+     */
+    public static void writeExcel(String path ,List<Object> list){
+
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("sheet1");
+            FileOutputStream out = new FileOutputStream(new File(path));//初始化输出流
+            if(list != null){
+                for(int i = 0; i<list.size();i++){
+                    List listrow = (List) list.get(i);
+                    XSSFRow row = sheet.createRow(i);
+                    for (int j = 0;j < listrow.size();j++){
+                        XSSFCell cell = row.createCell(j);
+                        cell.setCellValue((String) listrow.get(j));
+                    }
+                }
+            }
+            workbook.write(out);
+            workbook.close();//关闭excel文件操作
+            System.out.println("success");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    /**
+     * 向excel文件中写入数据,自定义开始行和列。不覆盖原则
+     * @param path 写入excel地址
+     * @param list excel数据
+     * @param rowstart 开始行号
+     * @param colstart 开始列号
+     */
+    public static void writeExcel(String path ,List<Object> list,int rowstart,int colstart){
+
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("sheet1");
+            FileOutputStream out = new FileOutputStream(new File(path));//初始化输出流
+            if(list != null){
+                for(int i = rowstart; i<list.size()+rowstart;i++){
+                    List listrow = (List) list.get(i-rowstart);
+                    XSSFRow row = sheet.createRow(i);
+                    for (int j = colstart;j < listrow.size()+colstart;j++){
+                        XSSFCell cell = row.createCell(j);
+                        cell.setCellValue((String) listrow.get(j-colstart));
+                    }
+                }
+            }
+            workbook.write(out);
+            workbook.close();//关闭excel文件操作
+            System.out.println("success");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
 
 }
